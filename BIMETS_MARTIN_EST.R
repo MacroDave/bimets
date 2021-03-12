026f85a1274ffb0922c5afe244702840b97690b6
MacroDave <david.stephan@gmail.com>
2021-03-12 16:39:51 -0300

    Now includes cash rate shock

--- BIMETS_MARTIN_EST.R
+++ BIMETS_MARTIN_EST.R
@@ -208,18 +208,140 @@ MARTINDATA <- list(
   Y=TIMESERIES(63799,64359,64683,66473,66623,66490,66690,65958,65486,66232,68091,69395,69957,71213,72857,71908,74859,76463,76341,78225,78710,80948,81627,82907,82665,82865,82626,83758,86126,86690,90073,89946,91637,92432,91609,95136,96393,99987,99294,101262,102937,105309,107497,109660,109432,109955,111284,111765,115277,114905,113511,116053,115464,116681,119694,119924,121129,124150,124174,121669,123130,123246,123734,127641,126243,124330,129829,130229,131285,132430,131709,133577,133007,132591,133526,134621,136663,137797,141568,139300,140594,143307,143951,144324,145084,147584,148154,150452,153476,152862,151623,152995,151973,149567,148065,147784,151907,154436,158313,160100,161455,162573,164941,168583,170873,170427,171551,171249,171729,174577,176249,179012,182267,185943,186749,186882,188424,191349,193317,197420,199021,198488,200122,200391,199208,200362,197773,197511,198330,198467,199951,201384,203423,207604,209044,210221,210444,214339,217928,220649,222332,224485,224421,225242,230359,230511,234369,236012,237901,240056,241547,248685,248995,252526,254242,256736,261685,265727,267784,268673,271809,276422,277467,280015,280554,279485,282341,284682,288107,291578,293777,298699,299741,301922,302237,303260,308311,313573,316068,318164,320598,323081,325612,327074,330563,333359,334052,334619,339615,343962,349094,351151,355047,356828,360879,361687,364490,362724,366480,368532,369659,372364,374245,376188,378805,382660,381630,386117,391327,395795,399487,402519,404855,407096,408186,410070,413324,416710,419555,421912,423890,425488,429046,429727,434298,436769,440554,443857,444431,448914,450243,453453,457683,459854,464085,468259,469394,469888,471974,475552,477958,START=c(1959,3),FREQ=4)
 )
 
-MARTIN=LOAD_MODEL("MARTINMOD_EST.txt")
+MARTIN=LOAD_MODEL("MARTINMOD_AF.txt")
 
 #load model data
 MARTIN=LOAD_MODEL_DATA(MARTIN,MARTINDATA)
 
 MARTIN=ESTIMATE(MARTIN)
 
-#simulate model
-MARTIN <- SIMULATE(MARTIN
-                 ,simType='DYNAMIC'
-                 ,TSRANGE=c(2017,2,2019,3)
-                 ,simConvergence=0.00001
-                 ,simIterLimit=100)
+#Assign Residuals as Add-Factors
+#define add-factor list
+constantAdjList <- list(
+  ATS = MARTIN$behaviorals$ATS$residuals,
+  DFD = MARTIN$behaviorals$DFD$residuals,
+  G = MARTIN$behaviorals$G$residuals,
+  GC = MARTIN$behaviorals$GC$residuals,
+  GI = MARTIN$behaviorals$GI$residuals,
+  GNE = MARTIN$behaviorals$GNE$residuals,
+  IAD = MARTIN$behaviorals$IAD$residuals,
+  IBCTR = MARTIN$behaviorals$IBCTR$residuals,
+  IBN = MARTIN$behaviorals$IBN$residuals,
+  IBNDR = MARTIN$behaviorals$IBNDR$residuals,
+  IBRE = MARTIN$behaviorals$IBRE$residuals,
+  IBREDR = MARTIN$behaviorals$IBREDR$residuals,
+  ID = MARTIN$behaviorals$ID$residuals,
+  IDDR = MARTIN$behaviorals$IDDR$residuals,
+  KIBN = MARTIN$behaviorals$KIBN$residuals,
+  KIBRE = MARTIN$behaviorals$KIBRE$residuals,
+  KID = MARTIN$behaviorals$KID$residuals,
+  KIDC = MARTIN$behaviorals$KIDC$residuals,
+  KV = MARTIN$behaviorals$KV$residuals,
+  LE = MARTIN$behaviorals$LE$residuals,
+  LHPP = MARTIN$behaviorals$LHPP$residuals,
+  LOKLAG = MARTIN$behaviorals$LOKLAG$residuals,
+  LPOP = MARTIN$behaviorals$LPOP$residuals,
+  LPR = MARTIN$behaviorals$LPR$residuals,
+  LUR = MARTIN$behaviorals$LUR$residuals,
+  M = MARTIN$behaviorals$M$residuals,
+  N10R = MARTIN$behaviorals$N10R$residuals,
+  N2R = MARTIN$behaviorals$N2R$residuals,
+  NATS = MARTIN$behaviorals$NATS$residuals,
+  NBRSP = MARTIN$behaviorals$NBRSP$residuals,
+  NCR = MARTIN$behaviorals$NCR$residuals,
+  NDFD = MARTIN$behaviorals$NDFD$residuals,
+  NGNE = MARTIN$behaviorals$NGNE$residuals,
+  NHC = MARTIN$behaviorals$NHC$residuals,
+  NHFA = MARTIN$behaviorals$NHFA$residuals,
+  NHL = MARTIN$behaviorals$NHL$residuals,
+  NHNFA = MARTIN$behaviorals$NHNFA$residuals,
+  NHOY = MARTIN$behaviorals$NHOY$residuals,
+  NHS = MARTIN$behaviorals$NHS$residuals,
+  NSD = MARTIN$behaviorals$NSD$residuals,
+  NSP = MARTIN$behaviorals$NSP$residuals,
+  NTWI = MARTIN$behaviorals$NTWI$residuals,
+  NULCBS = MARTIN$behaviorals$NULCBS$residuals,
+  NUSD = MARTIN$behaviorals$NUSD$residuals,
+  NV = MARTIN$behaviorals$NV$residuals,
+  OTC = MARTIN$behaviorals$OTC$residuals,
+  P = MARTIN$behaviorals$P$residuals,
+  PAE = MARTIN$behaviorals$PAE$residuals,
+  PC = MARTIN$behaviorals$PC$residuals,
+  PEX = MARTIN$behaviorals$PEX$residuals,
+  PG = MARTIN$behaviorals$PG$residuals,
+  PH = MARTIN$behaviorals$PH$residuals,
+  PI_E = MARTIN$behaviorals$PI_E$residuals,
+  PIBN = MARTIN$behaviorals$PIBN$residuals,
+  PIBRE = MARTIN$behaviorals$PIBRE$residuals,
+  PID = MARTIN$behaviorals$PID$residuals,
+  PM = MARTIN$behaviorals$PM$residuals,
+  PMCG = MARTIN$behaviorals$PMCG$residuals,
+  POTC = MARTIN$behaviorals$POTC$residuals,
+  PRT = MARTIN$behaviorals$PRT$residuals,
+  PTM = MARTIN$behaviorals$PTM$residuals,
+  PW = MARTIN$behaviorals$PW$residuals,
+  PXAG = MARTIN$behaviorals$PXAG$residuals,
+  PXM = MARTIN$behaviorals$PXM$residuals,
+  PXO = MARTIN$behaviorals$PXO$residuals,
+  PXRE = MARTIN$behaviorals$PXRE$residuals,
+  PXS = MARTIN$behaviorals$PXS$residuals,
+  RC = MARTIN$behaviorals$RC$residuals,
+  REWI = MARTIN$behaviorals$REWI$residuals,
+  RSTAR = MARTIN$behaviorals$RSTAR$residuals,
+  RTWI = MARTIN$behaviorals$RTWI$residuals,
+  RTWI_CONST = MARTIN$behaviorals$RTWI_CONST$residuals,
+  SD = MARTIN$behaviorals$SD$residuals,
+  TDLLA = MARTIN$behaviorals$TDLLA$residuals,
+  TDLLHPP = MARTIN$behaviorals$TDLLHPP$residuals,
+  TDLLPOP = MARTIN$behaviorals$TDLLPOP$residuals,
+  TLLA = MARTIN$behaviorals$TLLA$residuals,
+  TLLHPP = MARTIN$behaviorals$TLLHPP$residuals,
+  TLLPOP = MARTIN$behaviorals$TLLPOP$residuals,
+  TLUR = MARTIN$behaviorals$TLUR$residuals,
+  TY = MARTIN$behaviorals$TY$residuals,
+  V = MARTIN$behaviorals$V$residuals,
+  WP = MARTIN$behaviorals$WP$residuals,
+  WPAG = MARTIN$behaviorals$WPAG$residuals,
+  WPCOM = MARTIN$behaviorals$WPCOM$residuals,
+  WPOIL = MARTIN$behaviorals$WPOIL$residuals,
+  WPX = MARTIN$behaviorals$WPX$residuals,
+  WR2R = MARTIN$behaviorals$WR2R$residuals,
+  WRR = MARTIN$behaviorals$WRR$residuals,
+  WY = MARTIN$behaviorals$WY$residuals,
+  XAG = MARTIN$behaviorals$XAG$residuals,
+  XAGEXRAIN = MARTIN$behaviorals$XAGEXRAIN$residuals,
+  XM = MARTIN$behaviorals$XM$residuals,
+  XO = MARTIN$behaviorals$XO$residuals,
+  XRE = MARTIN$behaviorals$XRE$residuals,
+  XS = MARTIN$behaviorals$XS$residuals
+  
+)
 
-TABIT(((MARTIN$simulation$Y-MARTIN$modelData$Y)/MARTIN$modelData$Y)*100)
+#simulate model with add-factors set to equal residuals
+MARTIN <- SIMULATE(MARTIN
+                   ,TSRANGE=c(2010,1,2019,3)
+                   ,ConstantAdjustment=constantAdjList
+                  )
+
+#check historical and simulated Y and Z are equal
+TABIT((MARTIN$simulation$Y-MARTIN$modelData$Y)/MARTIN$modelData$Y*100)
+
+#shock the NCR add-factor on 2010q1
+Shock <- constantAdjList
+Shock$NCR[[2010,1]] <- Shock$NCR[[2010,1]] + 1
+Shock$NCR[[2010,2]] <- Shock$NCR[[2010,2]] + .341413
+Shock$NCR[[2010,3]] <- Shock$NCR[[2010,3]] + .427
+Shock$NCR[[2010,4]] <- Shock$NCR[[2010,4]] + .5137297
+
+MARTIN_Shocked <- SIMULATE(MARTIN,
+                              TSRANGE=c(2010,1,2019,3),
+                              ConstantAdjustment = Shock  )
+
+TABIT((MARTIN_Shocked$simulation$Y-MARTIN$simulation$Y)/MARTIN$simulation$Y*100)
+sim_chart <- as.data.frame((MARTIN_Shocked$simulation$Y-MARTIN$simulation$Y)/MARTIN$simulation$Y*100)
+sim_chart %>%
+  ggplot(aes(x = seq(0,38), y = x)) +
+  geom_line() + 
+  ylab("% deviation from baseline") +
+  xlab("Quarters")
+  
